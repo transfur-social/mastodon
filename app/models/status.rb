b1798d3ca4466ed0aeabcfcb57be70502946a49d
Claire <claire.github-309c@sitedethib.com>
2022-11-22 14:44:56 +0100

    Expose local account roles to the REST API

--- app/models/status.rb
+++ app/models/status.rb
@@ -116,7 +116,7 @@ class Status < ApplicationRecord
                    :tags,
                    :preview_cards,
                    :preloadable_poll,
-                   account: [:account_stat, :user],
+                   account: [:account_stat, user: :role],
                    active_mentions: { account: :account_stat },
                    reblog: [
                      :application,
@@ -126,7 +126,7 @@ class Status < ApplicationRecord
                      :conversation,
                      :status_stat,
                      :preloadable_poll,
-                     account: [:account_stat, :user],
+                     account: [:account_stat, user: :role],
                      active_mentions: { account: :account_stat },
                    ],
                    thread: { account: :account_stat }
